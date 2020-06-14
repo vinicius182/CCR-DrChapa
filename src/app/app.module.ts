ad32abbfa2a121f250ef2546d6b2f67220f59505
Evandro <evandro.ignacio@fcamara.com.br>
2020-06-14 12:22:12 -0300

    [ADD] foi adicionado o HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
