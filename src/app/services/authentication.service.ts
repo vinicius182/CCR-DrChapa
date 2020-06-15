79acc5f8e8a2b59e0a32630b7c778e4330cfb3ec
vinicius.dias <vinicius.ferreirad@hotmail.com>
2020-06-14 22:40:50 -0300

    login

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -31,43 +31,42 @@ export class AuthenticationService {
    }
 
    login() {
-    var headers = new HttpHeaders({
-      'Content-Type' : 'application/json; charset=utf-8',
-        'Accept': 'application/json, text/plain',
-        "cache-control": "no-cache", 
-        "Access-Control-Allow-Origin": "*", 
-        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Accept, Authorization, X-Request-With, Access-Control-Request-Method, Access-Control-Request-Headers",
-        "Access-Control-Allow-Credentials" : "true",
-        "Access-Control-Allow-Methods" : "GET, POST, DELETE, PUT, OPTIONS, TRACE, PATCH, CONNECT",  
-    });
+  //   var headers = new HttpHeaders({
+  //     'Content-Type' : 'application/json; charset=utf-8',
+  //       'Accept': 'application/json, text/plain',
+  //       "cache-control": "no-cache", 
+  //       "Access-Control-Allow-Origin": "*", 
+  //       "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Accept, Authorization, X-Request-With, Access-Control-Request-Method, Access-Control-Request-Headers",
+  //       "Access-Control-Allow-Credentials" : "true",
+  //       "Access-Control-Allow-Methods" : "GET, POST, DELETE, PUT, OPTIONS, TRACE, PATCH, CONNECT",  
+  //   });
 
-     var loginForm = {
-      username: '44992687883',
-      password: '123'
-     };
+  //    var loginForm = {
+  //     username: '44992687883',
+  //     password: '123'
+  //    };
 
-     let requestOptions = {
-      headers: headers
-   }
+  //    let requestOptions = {
+  //     headers: headers
+  //  }
 
-     this.httpClient.post('http://192.168.0.22:8080/api/v1/auth', loginForm, requestOptions)
-     .subscribe(data => {
-       console.log(data)
-       this.storage.set('USER_INFO', data).then((reponse) => {
-         this.router.navigate(['tabs/viagem']);
-         this.authState.next(true);
-       })
-     })
+  //    this.httpClient.post('http://192.168.0.22:8080/api/v1/auth', loginForm, requestOptions)
+  //    .subscribe(data => {
+  //      console.log(data)
+  //      this.storage.set('USER_INFO', data).then((reponse) => {
+  //        this.router.navigate(['tabs/viagem']);
+  //        this.authState.next(true);
+  //      })
+  //    })
 
-    // DESCOMENTAR E COMENTAR AS COISAS DE CIMA CASO LOGIN NAO FUNCIONE 
-      // var dummy_response = {
-      // user_id: '007',
-      // user_name: 'test'
-      // };
-      // this.storage.set('USER_INFO', dummy_response).then((response) => {
-      //   this.router.navigate(['dashboard']);
-      //   this.authState.next(true);
-      // });
+      var dummy_response = {
+      user_id: '007',
+      user_name: 'test'
+      };
+      this.storage.set('USER_INFO', dummy_response).then((response) => {
+        this.router.navigate(['dashboard']);
+        this.authState.next(true);
+      });
    }
 
    logout() {
